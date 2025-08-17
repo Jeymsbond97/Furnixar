0d97a5a948afe13a6ff5c89ed1dabca83c3d7980
Jeymsbond97 <aminjonovtoxirbek@gmail.com>
2025-08-17 19:54:41 +0900

    fix: modify deploy.sh file and update

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 
 # PRODUCTION
 
+nvm use 20 || nvm install 20 && nvm use 20
+
 git reset --hard
 git checkout master
 git pull origin master
 
-npm i
+npm i --legacy-peer-deps
 npm run build
 pm2 start "npm run start:prod" --name=FURNIXAR-REACT
